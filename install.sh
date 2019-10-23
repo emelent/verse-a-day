644702a6d66b407ea92431d388fd89c14ac47da4
Roy Motloutsi <roy.motloutsi@gmail.com>
2019-10-23 17:34:16 +0200

    Add installation script

--- install.sh
+++ install.sh
@@ -3,10 +3,10 @@
 check=`grep "setup_bible_verses" .bashrc &> /dev/null`
 if [ "$check" = "" ]; then
     echo "Setting up .bashrc bible script..."
-    text=`curl -s https://gist.githubusercontent.com/emelent/6a18bb8015664877dc605baa40cd10d3/raw/23a54e5419db4c4db7bff43db196c6f3d0c6119c/setup_bible_verses.sh` &&
-    echo $text >> .bashrc &&
-    echo "\nGreat, your .bashrc is now setup for bible verses. 😁" ||
-    echo "Setup failed, make sure you're online then try again. 😕"
+    text=$(curl -s https://raw.githubusercontent.com/emelent/verse-a-day/master/bashrc) &&
+        echo $text >> .bashrc &&
+        echo "\nGreat, your .bashrc is now setup for bible verses. 😁" ||
+        echo "Setup failed, make sure you're online then try again. 😕"
 else
     echo "Your .bashrc is already setup for bible verses. 😋"
 fi
