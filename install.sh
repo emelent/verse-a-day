0e088214af5d933b2476ca47a669a9758727d606
Roy Motloutsi <roy.motloutsi@gmail.com>
2019-10-24 10:51:13 +0200

    Add missing '&&'

--- install.sh
+++ install.sh
@@ -4,7 +4,7 @@ check=$(grep "setup_bible_verses" ~/.bashrc &> /dev/null)
 if [ "$check" = "" ]; then
     echo "Setting up .bashrc bible script..."
     curl -s https://raw.githubusercontent.com/emelent/verse-a-day/master/bashrc >> ~/.bashrc &&
-        echo
+        echo &&
         echo "Great, your .bashrc is now setup for bible verses. 😁" ||
         echo "Setup failed, make sure you're online then try again. 😕"
 else
